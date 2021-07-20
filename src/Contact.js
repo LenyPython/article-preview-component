4bbc834ccef1a6b936c941410c0ed156df3d9adf
LenyPython <buniadyserv@gmail.com>
2021-07-20 20:11:49 +0200

    added share element to contact, need to add fuctionality to it to hide it and change button style

--- src/Contact.js
+++ src/Contact.js
@@ -1,10 +1,29 @@
 import React from 'react'
 import arrow from './images/icon-share.svg'
+import fb from './images/icon-facebook.svg'
+import pin from './images/icon-pinterest.svg'
+import twitter from './images/icon-twitter.svg'
 
 const Contact = () => {
+
+  const handleClick = () => {
+    let div = document.querySelector('contact');
+    let menu = document.getElementById('share');
+    div.classList.toggle('clicked');
+    menu.classList.toggle('visible');
+
+  };
+
   return (
-    <div className='contact'>
+    <div className='contact' onClick={handleClick}>
       <img src={arrow} alt='arrow' />
+      <div id='share'>
+        <h2>share</h2>
+        <img src={fb} alt='fb-icon' />
+        <img src={twitter} alt='tw-icon' />
+        <img src={pin} alt='pin-icon' />
+
+      </div>
     </div>
   )
 }
