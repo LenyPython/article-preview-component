7b26f5ad857f2c6846b0e5e6ecfd4cf0b95f294d
LenyPython <buniadyserv@gmail.com>
2021-07-23 20:49:46 +0200

    changed ids to classes of contac component. Added fuctionality on click to add new classes to the elements.

--- src/Contact.js
+++ src/Contact.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import arrow from './images/icon-share.svg'
 import fb from './images/icon-facebook.svg'
 import pin from './images/icon-pinterest.svg'
 import twitter from './images/icon-twitter.svg'
@@ -7,17 +6,16 @@ import twitter from './images/icon-twitter.svg'
 const Contact = () => {
 
   const handleClick = () => {
-    let div = document.querySelector('contact');
-    let menu = document.getElementById('share');
-    div.classList.toggle('clicked');
-    menu.classList.toggle('visible');
+    let contactArrow = document.querySelector('.contact');
+    let shareDiv = document.querySelector('.share');
+    contactArrow.classList.toggle('clicked');
+    shareDiv.classList.toggle('visible');
 
   };
 
   return (
     <div className='contact' onClick={handleClick}>
-      <img src={arrow} alt='arrow' />
-      <div id='share'>
+      <div className='share'>
         <h2>share</h2>
         <img src={fb} alt='fb-icon' />
         <img src={twitter} alt='tw-icon' />
