24c10fb95f6718c1b40b08cb76bc67dc78c3e019
LenyPython <buniadyserv@gmail.com>
2021-07-28 17:41:12 +0200

    recostructed shar div. moved it to upper in hierarhy, so it will inherit from parent main container. adde css style for media < 1050px for rwd.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,5 @@
 import React from 'react'
 import './contact.scss'
-import fb from '../images/icon-facebook.svg'
-import pin from '../images/icon-pinterest.svg'
-import twitter from '../images/icon-twitter.svg'
 import arrow from '../images/icon-share.svg'
 
 const Contact = () => {
@@ -18,13 +15,6 @@ const Contact = () => {
   return (
     <div className='contact' onClick={handleClick}>
       <img src={arrow} alt='click arrow' />
-      <div className='share'>
-        <h2>share</h2>
-        <img src={fb} alt='fb-icon' />
-        <img src={twitter} alt='tw-icon' />
-        <img src={pin} alt='pin-icon' />
-
-      </div>
     </div>
   )
 }
--- src/components/Personal.js
+++ src/components/Personal.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import Contact from './Contact'
+import fb from '../images/icon-facebook.svg'
+import pin from '../images/icon-pinterest.svg'
+import twitter from '../images/icon-twitter.svg'
 
 const Personal = ({name, date, avatar}) => {
   return (
@@ -10,6 +13,12 @@ const Personal = ({name, date, avatar}) => {
           <h2>{name}</h2>
           <p>{date}</p>
         </div>
+      </div>
+      <div className='share'>
+        <h2>share</h2>
+        <img src={fb} alt='fb-icon' />
+        <img src={twitter} alt='tw-icon' />
+        <img src={pin} alt='pin-icon' />
       </div>
       <Contact />
     </div>
